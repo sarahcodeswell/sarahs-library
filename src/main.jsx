bb9f713b4d670b5ee2af5ebe9c2b1bbda9dc4adf
Sarah Ritter <sarah@workato.com>
2025-12-27 16:35:29 -0800

    Fix: Remove UserBooksProvider to fix Add Books page error

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { UserProvider, ReadingQueueProvider, UserBooksProvider, RecommendationProvider } from './contexts'
+import { UserProvider, ReadingQueueProvider, RecommendationProvider } from './contexts'
 import ErrorBoundary from './components/ErrorBoundary'
 import { initSentry } from './lib/sentry'
 
@@ -14,11 +14,9 @@ createRoot(document.getElementById('root')).render(
     <ErrorBoundary>
       <UserProvider>
         <ReadingQueueProvider>
-          <UserBooksProvider>
-            <RecommendationProvider>
-              <App />
-            </RecommendationProvider>
-          </UserBooksProvider>
+          <RecommendationProvider>
+            <App />
+          </RecommendationProvider>
         </ReadingQueueProvider>
       </UserProvider>
     </ErrorBoundary>
